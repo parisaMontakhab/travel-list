14ae5d01fefd582abfd2309aed382c07442a8337
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-19 12:17:19 +0200

    feat: add const in App

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,5 +1,10 @@
 
-import './App.css'
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 
 function App() {
   
