6f9f07404c54a7b635ef6ccb82ccc5366eb2cfe9
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-23 13:06:19 +0200

    feat: add sort method in pakinglist

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -79,10 +79,20 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDelet, onToggleItem }) {
+  const [sortBy,setSortBy] = useState('input');
+  let sortedItem ;
+  if(sortBy === 'input') sortedItem=items
+
+  if(sortBy === 'description') sortedItem =items.slice().sort((a,b)=>a.desc.localeCompare(b.desc))
+  
+  if(sortBy === 'packed') sortedItem = items.slice().sort((a,b)=> Number(a.packed) - Number(b.packed)) 
+
+
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItem.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -91,6 +101,13 @@ function PackingList({ items, onDelet, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e)=> setSortBy(e.target.value)}>
+          <option value='input'>sort by input order</option>
+          <option value='description'>sort by description</option>
+          <option value='packed'>sort by packed status </option>
+        </select>
+      </div>
     </div>
   );
 }
